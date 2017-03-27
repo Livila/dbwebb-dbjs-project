8b8e9a28223d91c88e832f6ff4fbb357c46f5dc8
Jonathan Ström <jonathan.c.strom@gmail.com>
2017-03-27 14:40:28 +0200

    Minor konsistency fix.

--- CLI/Internetbanken.sql
+++ CLI/Internetbanken.sql
@@ -31,23 +31,23 @@ CREATE TABLE UserAccount (
 );
 
 INSERT INTO User
-	(pinCode, civicNumber, firstName, lastName, street, city)
+    (pinCode, civicNumber, firstName, lastName, street, city)
 VALUES
-	(1234, '198804131234', 'Oskar', 'Art', 'Oskargatan', 'Oskarstaden'),
-	(1234, '198906131111', 'Olle', 'Art', 'Oskargatan', 'Oskarstaden');
+    (1234, '198804131234', 'Oskar', 'Art', 'Oskargatan', 'Oskarstaden'),
+    (1234, '198906131111', 'Olle', 'Art', 'Oskargatan', 'Oskarstaden');
 
 INSERT INTO Account
-	(accountNr, balance)
+    (accountNr, balance)
 VALUES
-	('1234567812345678', 0),
-	('1130491238512304', 0);
+    ('1234567812345678', 100),
+    ('1130491238512304', 14000);
 
 INSERT INTO UserAccount
-	(userId, accountId)
+    (userId, accountId)
 VALUES
-	(1, 1),
-	(2, 2),
-	(1, 2);
+    (1, 1),
+    (2, 2),
+    (1, 2);
 
 CREATE VIEW VUserAndAccount AS
 SELECT User.firstName, User.lastName, Account.balance, Account.accountId
